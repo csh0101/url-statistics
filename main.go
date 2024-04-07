9dca44b547a84367a134d1e3a73893f6207e3814
csh0101 <1945598041@qq.com>
2024-04-07 09:35:25 +0800

    request support

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"net/url"
 	"os"
 	"strings"
+
+	"github.com/imroc/req/v3"
 )
+
+var (
+	gl2WhileAPI string
+)
+
+func init() {
+	gl2WhileAPI = os.Getenv("GL2_WHILE_API")
+}
 
 func readText() []string {
 
@@ -64,6 +76,8 @@ func main() {
 		}
 	}
 
+	SyncGL2Api(urlPathMap)
+
 	fmt.Println(len(urlPathMap[NC_CLI_IM]))
 	fmt.Println(len(urlPathMap[CLI_IM]))
 	fmt.Println(len(urlPathMap[USER_CLI_IM]))
@@ -81,4 +95,83 @@ func isUnique(uniqueMap map[string]struct{}, key string) bool {
 
 }
 
-// func
+func SyncGL2Api(urlPathMap map[string][]string) {
+	for app, uris := range urlPathMap {
+		for _, uri := range uris {
+			err := DealWithAppPair(uri, app)
+
+			if err != nil {
+				fmt.Println(err.Error())
+				continue
+			}
+
+		}
+	}
+
+}
+
+func DealWithAppPair(uri string, app string) error {
+
+	client := req.NewClient()
+
+	resp, err := client.NewRequest().SetQueryParams(map[string]string{
+		"app": app,
+		"uri": uri,
+	}).Get(gl2WhileAPI + "/api/uri/app_uri/verify")
+
+	if err != nil {
+		return err
+	}
+
+	if resp.GetStatusCode() != 200 {
+		return errors.New("status code is not 200")
+	}
+
+	bizResp := &BizResp{}
+
+	err = json.Unmarshal(resp.Bytes(), resp)
+
+	if err != nil {
+		return err
+	}
+
+	if !bizResp.Success {
+		resp, err := client.NewRequest().SetBody(AddUriPair{
+			App: app,
+			Uri: uri,
+		}).Post(gl2WhileAPI + "/api/uri/app_uri")
+
+		if err != nil {
+			return err
+		}
+
+		if resp.GetStatusCode() != 200 {
+			return errors.New("status code is not 200")
+		}
+
+		bizResp = &BizResp{}
+
+		err = json.Unmarshal(resp.Bytes(), resp)
+
+		if err != nil {
+			return err
+		}
+
+		if !bizResp.Success {
+			return fmt.Errorf("add app url error,app: %s , url: %s", app, uri)
+		}
+	}
+
+	return nil
+}
+
+type BizResp struct {
+	Data    interface{} `json:"data"`
+	Message string      `json:"message"`
+	Success bool        `json:"success"`
+}
+
+type AddUriPair struct {
+	App string `json:"app"`
+	Uri string `json:"uri"`
+}
